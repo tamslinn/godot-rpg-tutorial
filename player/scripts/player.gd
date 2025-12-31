c0c016c883c2813bdc99ebf612724348c87b3466
Tamsin <tamsin.slinn@irata.com>
2025-12-31 14:04:20 +0000

    Tutorial Part 1

---
+++ player/scripts/player.gd
@@ -0,0 +1,18 @@
+class_name Player extends CharacterBody2D
+
+var move_speed : float = 100.0
+
+func _ready() -> void:
+	pass
+	
+func _process(_delta: float) -> void:
+	var direction : Vector2 = Vector2.ZERO
+	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+	velocity = direction * move_speed
+	
+	pass
+	
+func _physics_process(_delta: float) -> void:
+	move_and_slide()
